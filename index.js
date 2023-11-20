1fa2e331c45b09e2c5928488e06ad0a379974392
estan <elise.stankus@gmail.com>
2023-11-19 18:12:25 -0800

    added questions to array

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter instructions for installation.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the usage information for your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select the license for your project.',
+        choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'none'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter any contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter any test instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
