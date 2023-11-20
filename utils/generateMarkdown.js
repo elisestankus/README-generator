52ce4590687353bb0b1429bbed3d65ed1c8f549b
estan <elise.stankus@gmail.com>
2023-11-19 20:24:04 -0800

    added renderLicenseBadge code and called in generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+
+  if (data.license == 'Apache 2.0') {
+    badge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  } else if (data.license == 'MIT'){
+    badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (data.license == 'GPLv2') {
+    badge = `![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)`
+  } else if (data.license == 'GPLv3') {
+    badge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (data.license == 'BSD 3-clause') {
+    badge = `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+  } else {
+    badge = ''
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +28,8 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  renderLicenseBadge(data)
+  return `# ${data.title}   ${badge}
 
   ## Description
 
@@ -62,3 +79,6 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+
+
