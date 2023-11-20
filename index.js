a8df64dc1312e130de5353b928b89a8caad769f3
estan <elise.stankus@gmail.com>
2023-11-19 18:14:00 -0800

    added required packages and generateMarkdown

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
