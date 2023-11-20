faf21b7a11e9772a7e19f1b0d90c334ccc3fbee6
estan <elise.stankus@gmail.com>
2023-11-19 21:09:32 -0800

    added renderLicenseSection code and called

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -38,11 +38,19 @@ function renderLicenseLink(data) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  if (data.license == 'none') {
+    licenseSection = 'N/A'
+  } else {
+    licenseSection = `This application is covered under a(n) <a href = "${licenseLink}">${data.license}</a> license`
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   renderLicenseLink(data)
+  renderLicenseSection(data)
   renderLicenseBadge(data)
   return `# ${data.title}      <a href = "${licenseLink}">${badge}</a>
 
@@ -74,7 +82,7 @@ function generateMarkdown(data) {
 
   ## License 
 
-  ${data.license}
+  ${licenseSection}
 
   ## Contributing 
 
